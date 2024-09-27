86c88c2b0d480e7b30e05d297e13098e087930ec
luca-vigano <luca.vigano@hotmail.com>
2024-09-27 16:28:01 +0200

    player

--- src/ElementoMultimediale.java
+++ src/ElementoMultimediale.java
@@ -1,8 +1,23 @@
-public abstract class ElementoMultimediale {
+import javax.swing.*;
+import java.awt.*;
+
+public abstract class ElementoMultimediale{
 
     String titolo;
 
     public ElementoMultimediale(String titolo) {
         this.titolo = titolo;
     }
+
+    static void method (ElementoMultimediale em){
+        if (em instanceof Audio) {
+            Audio a = (Audio) em;
+            a.Play();
+        } else if (em instanceof Video) {
+            Video v = (Video) em;
+            v.Play();
+        }else if (em instanceof Immagine) {
+            Immagine i = (Immagine) em;
+            i.Show();
+    }}
 }
--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,20 @@ public class Main {
         System.out.println("Inserisci il titolo della prima traccia audio: ");
         String titoloAudio1 = scanner.nextLine();
         System.out.println("Inserisci la durata della prima traccia audio: ");
-        int durataAudio1 = scanner.nextInt();
+        int durataAudio1 = Integer.parseInt(scanner.nextLine());
         System.out.println("Inserisci il volume della prima traccia audio: ");
-        int volumeAudio1 = scanner.nextInt();
-        scanner.nextLine();
+        int volumeAudio1 = Integer.parseInt(scanner.nextLine());
+
 
         Audio track1 = new Audio(titoloAudio1, durataAudio1, volumeAudio1);
 
         System.out.println("Inserisci il titolo della seconda traccia audio: ");
         String titoloAudio2 = scanner.nextLine();
         System.out.println("Inserisci la durata della seconda traccia audio: ");
-        int durataAudio2 = scanner.nextInt();
+        int durataAudio2 = Integer.parseInt(scanner.nextLine());
         System.out.println("Inserisci il volume della seconda traccia audio: ");
-        int volumeAudio2 = scanner.nextInt();
-        scanner.nextLine();
+        int volumeAudio2 = Integer.parseInt(scanner.nextLine());
+
 
         Audio track2 = new Audio(titoloAudio2, durataAudio2, volumeAudio2);
 
@@ -29,31 +29,31 @@ public class Main {
         System.out.println("Inserisci il titolo del primo video: ");
         String titoloVideo1 = scanner.nextLine();
         System.out.println("Inserisci la durata del primo video: ");
-        int durataVideo1 = scanner.nextInt();
+        int durataVideo1 = Integer.parseInt(scanner.nextLine());
         System.out.println("Inserisci il volume del primo video: ");
-        int volumeVideo1 = scanner.nextInt();
+        int volumeVideo1 = Integer.parseInt(scanner.nextLine());
         System.out.println("Inserisci la luminosità del primo video: ");
-        int luminositaVideo1 = scanner.nextInt();
-        scanner.nextLine();
+        int luminositaVideo1 = Integer.parseInt(scanner.nextLine());
+
 
         Video video1 = new Video(titoloVideo1, durataVideo1, volumeVideo1, luminositaVideo1);
 
         System.out.println("Inserisci il titolo del secondo video: ");
         String titoloVideo2 = scanner.nextLine();
         System.out.println("Inserisci la durata del secondo video: ");
-        int durataVideo2 = scanner.nextInt();
+        int durataVideo2 = Integer.parseInt(scanner.nextLine());
         System.out.println("Inserisci il volume del secondo video: ");
-        int volumeVideo2 = scanner.nextInt();
+        int volumeVideo2 = Integer.parseInt(scanner.nextLine());
         System.out.println("Inserisci la luminosità del secondo video: ");
-        int luminositaVideo2 = scanner.nextInt();
-        scanner.nextLine();
+        int luminositaVideo2 = Integer.parseInt(scanner.nextLine());
+
 
         Video video2 = new Video(titoloVideo2, durataVideo2, volumeVideo2, luminositaVideo2);
 
         System.out.println("Inserisci il titolo della prima immagine: ");
         String titoloImg1 = scanner.nextLine();
         System.out.println("Inserisci la luminosità della prima immagine: ");
-        int luminositaImg1 = scanner.nextInt();
+        int luminositaImg1 = Integer.parseInt(scanner.nextLine());
 
         Immagine img1 = new Immagine(titoloImg1, luminositaImg1);
 
@@ -62,31 +62,21 @@ public class Main {
         System.out.println("Comandi: 1-2 audio, 3-4 video, 5 img. exit: termina player");
         System.out.println("Inserisci un comando ");
 
+        ElementoMultimediale[] multimediaArr = {track1, track2, video1, video2, img1};
 
         boolean attivo =true;
         while (attivo){
 
             String comando = scanner.nextLine();
             switch (comando){
-                case "1":
-                    track1.Play();
-                    break;
-                case "2":
-                    track2.Play();
-                    break;
-                case "3":
-                    video1.Play();
-                    break;
-                case "4":
-                    video2.Play();
-                    break;
-                case "5":
-                    img1.Show();
+                case "1", "2", "3", "4", "5":
+                   ElementoMultimediale.method(multimediaArr[Integer.parseInt(comando)-1]);
                     break;
                 case "exit":
                     attivo=false;
                     break;
-
+                default:
+                    System.out.println("Comando non valido");
             }
         }
 
